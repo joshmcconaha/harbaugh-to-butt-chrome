c842119d7804e6fb334675d1acf8384a5fadb316
Will Stamper <epmatsw@gmail.com>
2015-03-16 10:36:11 -0400

    Tweak Replace Calls
    Slight changes:
    1. Removed 3 replace calls by adding "jim " as an optional substring.
    2. Made the last check case-insensitive so things like "Jim harbaugh" and "jIm harbaugh" will get replaced.
    3. Declared the regexes outside of the function. This allows for slightly better performance.
    4. Minor code formatting changes for JSLint.
    
    [JSPerf for comparison](http://jsperf.com/harbaugh)

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,44 +1,39 @@
+var normalCapsRegex = /\b(Jim )?Harbaugh\b/g;
+var allCapsRegex = /\b(JIM )?HARBAUGH\b/g;
+var caseInsensitiveRegex = /\b(jim )?harbaugh\b/gi;
+
+function handleText(textNode) {
+    var v = textNode.nodeValue;
+
+    v = v.replace(normalCapsRegex, "My Butt");
+    v = v.replace(allCapsRegex, "MY BUTT");
+    v = v.replace(caseInsensitiveRegex, "my butt");
+
+    textNode.nodeValue = v;
+}
+
+function walk(node) {
+    // I stole this function from here:
+    // http://is.gd/mwZp7E
+
+    var child, next;
+
+    switch (node.nodeType) {
+        case 1: // Element
+        case 9: // Document
+        case 11: // Document fragment
+            child = node.firstChild;
+            while (child) {
+                next = child.nextSibling;
+                walk(child);
+                child = next;
+            }
+            break;
+
+        case 3: // Text node
+            handleText(node);
+            break;
+    }
+}
+
 walk(document.body);
-
-function walk(node) 
-{
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
-	
-	var child, next;
-
-	switch ( node.nodeType )  
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
-
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
-}
-
-function handleText(textNode) 
-{
-	var v = textNode.nodeValue;
-	
-	v = v.replace(/\bJim Harbaugh\b/g, "my butt");
-	v = v.replace(/\bjim harbaugh\b/g, "my butt");
-	v = v.replace(/\bJIM HARBAUGH\b/g, "MY BUTT");
-	v = v.replace(/\bHarbaugh\b/g, "My Butt");
-	v = v.replace(/\bHARBAUGH\b/g, "MY BUTT");
-	v = v.replace(/\bharbaugh\b/g, "my butt");
-	
-	textNode.nodeValue = v;
-}
-
-
